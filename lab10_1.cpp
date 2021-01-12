dbb3617b69d07609b53536dd61721f13635db8fc
NATKAMONLAK <paintlpch@gmail.com>
2021-01-12 15:41:29 +0700

    completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    int i = rand()%8;
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.\n";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get "<<grade[i]<<" in this 261102.";
+    return 0;
+}
